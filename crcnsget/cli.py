455bbacf71f9ebafc63fbf34b3ec9f7d8c6b0793
Justin Kiggins <justin.kiggins@gmail.com>
2018-03-18 13:50:27 -0700

    first pass at loading password from env

--- crcnsget/cli.py
+++ crcnsget/cli.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import click
-import crcnsget
+from .crcnsget import fetch_crcns_datafile
 
 @click.command()
 @click.option('--username', prompt='Your crcns.org username',
-              help='Your crcns.org username.')
+              help='Your crcns.org username.', default=None)
 @click.option('--password', prompt='Your crcns.org password',
               help='Your crcns.org password.',
-              hide_input=True)
+              hide_input=True, default=None)
 @click.option('--dataset', prompt='Path to a crcns.org dataset',
               help='Your crcns.org dataset.')
 def main(username,password,dataset):
     """Console script for crcnsget"""
-    crcnsget.download(dataset,username,password)
+    fetch_crcns_datafile(dataset,username,password)
 
 if __name__ == "__main__":
     main()
--- crcnsget/crcnsget.py
+++ crcnsget/crcnsget.py
@@ -4,20 +4,51 @@ import requests
 
 URL = 'https://portal.nersc.gov/project/crcns/download/index.php'
 
-def download(datafile,username,password):
 
-    login_data = dict(
+def create_payload(username,password,datafile):
+    return dict(
         username=username,
         password=password,
         fn=datafile,
-        submit='Login' 
-        )
+        submit='Login'
+    )
+
+def get_environ_username():
+    return os.environ['CRCNS_USER']
+
+def get_environ_password():
+    return os.environ['CRCNS_PASSWORD']
+
+def create_local_filename(dest,datafile):
+    if dest is None:
+        dest = os.cwd()
+    return os.path.join(
+        dest,
+        datafile.split('/')[-1],
+    )
+
+def download(session,request_payload,local_filename):
+    r = session.post(URL,data=request_payload,stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return local_filename
+
+def fetch_crcns_datafile(datafile,dest=None,username=None,password=None,chunk_size=1024):
+
+    local_filename = create_local_filename(dest,datafile)
+
+    if username is None:
+        username = get_environ_username()
+    if password is None:
+        password = get_environ_password()
+
+    request_payload = create_payload(
+        username,
+        password,
+        datafile,
+    )
 
     with requests.Session() as s:
-        local_filename = login_data['fn'].split('/')[-1]
-        r = s.post(URL,data=login_data,stream=True)
-        with open(local_filename, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=1024):
-                if chunk:
-                    f.write(chunk)
-        print local_filename
+        download_file(s,request_payload,local_filename)
