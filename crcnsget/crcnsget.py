6d5a6faba774cefdabd656df6170b5a52296f620
HyunGeun Lee <52870444+hglee98@users.noreply.github.com>
2023-11-11 10:46:08 +0900

    Update crcnsget.py
    This commit aims for adding feature of progress bar using tqdm

--- crcnsget/crcnsget.py
+++ crcnsget/crcnsget.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+from tqdm import tqdm
 
 URL = 'https://portal.nersc.gov/project/crcns/download/index.php'
 
@@ -16,8 +17,16 @@ def download(datafile,username,password):
     with requests.Session() as s:
         local_filename = login_data['fn'].split('/')[-1]
         r = s.post(URL,data=login_data,stream=True)
-        with open(local_filename, 'wb') as f:
+        total_size = int(r.headers.get('Content-Length', 0))
+
+        with open(local_filename, 'wb') as f, tqdm(
+                desc=local_filename,
+                total=total_size,
+                unit='B',
+                unit_scale=True,
+                unit_divisor=1024) as bar:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:
                     f.write(chunk)
-        print(local_filename)
+                    bar.update(len(chunk))
+        print(f"{local_filename} downloaded successfully.")
